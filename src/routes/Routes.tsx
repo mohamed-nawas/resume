ae4cfb62d2315c134d5dd58c71eda0cf3e8f2f70
mohamed-nawas <mgmnawas@gmail.com>
2025-04-23 18:28:41 +0530

    fix 2

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,15 +1,33 @@
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, HashRouter, BrowserRouter, Switch } from "react-router-dom";
 import App from "../App";
 import Contact from "../pages/contact";
 
 export default function Routes() {
-    return (
-        <Router>
+    const isProd = import.meta.env.PROD;
+
+    // return (
+    //     <RouterWrapper>
+    //         <Switch>
+    //             <Route exact path="/" component={App} />
+    //             <Route exact path="/contact" component={Contact} />
+    //             {/* <Route path="/login" component={Auth} /> */}
+    //         </Switch>
+    //     </RouterWrapper>
+    // )
+    return isProd ? (
+        <HashRouter>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route exact path="/contact" component={Contact} />
+            </Switch>
+        </HashRouter>
+    ) : (
+        <BrowserRouter>
             <Switch>
                 <Route exact path="/resume" component={App} />
                 <Route exact path="/resume/contact" component={Contact} />
                 {/* <Route path="/login" component={Auth} /> */}
             </Switch>
-        </Router>
+        </BrowserRouter>
     )
 }
