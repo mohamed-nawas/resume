8bc5decc9d99b124807bc1df9d7129bd16bd96b4
mohamed-nawas <mgmnawas@gmail.com>
2025-04-28 17:16:52 +0530

    routing enhancement

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { Route, HashRouter, BrowserRouter, Switch } from "react-router-dom";
+import { Route, HashRouter, BrowserRouter, Switch, Redirect } from "react-router-dom";
 import App from "../App";
 import Contact from "../pages/contact";
 import About from "../pages/about";
@@ -7,9 +7,14 @@ import Portfolio from "../pages/portfolio";
 export default function Routes() {
     const isProd = import.meta.env.PROD;
     return isProd ? (
+        // HashRouter is used for production to avoid issues with routing
+        // when the app is served from a subdirectory
+        // HashRouter uses the hash portion of the URL (window.location.hash)
+        // to keep the UI in sync with the URL
         <HashRouter>
             <Switch>
-                <Route exact path="/" component={App} />
+                <Route exact path="/"><Redirect to="/resume" /></Route>
+                <Route exact path="/resume" component={App} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/projects" component={Portfolio} />
                 <Route exact path="/contact" component={Contact} />
