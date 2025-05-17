bf1020826e26ae7e3aa897cb27bf9f94e8600686
mohamed-nawas <mgmnawas@gmail.com>
2025-05-17 20:48:57 +0530

    fixed expand for slider image bug

--- src/data/case-studies.tsx
+++ src/data/case-studies.tsx
@@ -1,17 +1,20 @@
 export const items = [
     {
+        id: 'case-study-001',
         title: "Swivel Group",
         mainImgPath: "images/swivel-group-image-1.png",
         subImgsPath: ["images/swivel-group-image-1.png", "images/swivel-group-image-2.png", "images/swivel-group-image-3.png", "images/swivel-group-image-4.png"],
         imgAlt: "Swivel Group Image",
     },
     {
+        id: 'case-study-002',
         title: "Swivel Group",
         mainImgPath: "images/swivel-group-image-1.png",
         subImgsPath: ["images/swivel-group-image-1.png", "images/swivel-group-image-2.png", "images/swivel-group-image-3.png", "images/swivel-group-image-4.png"],
         imgAlt: "Swivel Group Image",
     },
     {
+        id: 'case-study-003',
         title: "Swivel Group",
         mainImgPath: "images/swivel-group-image-1.png",
         subImgsPath: ["images/swivel-group-image-1.png", "images/swivel-group-image-2.png", "images/swivel-group-image-3.png", "images/swivel-group-image-4.png"],
