f0bcc02236c96135a543baf9d5639d812e14279d
mohamed-nawas <mgmnawas@gmail.com>
2025-05-18 01:48:14 +0530

    detail page dev done, little fix remaining

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,33 +1,27 @@
+import * as React from 'react';
 import { Route, HashRouter, BrowserRouter, Switch } from "react-router-dom";
 import App from "../App";
 import Contact from "../pages/contact";
 import About from "../pages/about";
 import Portfolio from "../pages/portfolio";
+import Detail from "../pages/detail";
+
+
+const isProd = import.meta.env.PROD;
+const Router = isProd ? HashRouter : BrowserRouter;
 
 export default function Routes() {
-    const isProd = import.meta.env.PROD;
-    return isProd ? (
-        // HashRouter is used for production to avoid issues with routing
-        // when the app is served from a subdirectory
-        // HashRouter uses the hash portion of the URL (window.location.hash)
-        // to keep the UI in sync with the URL
-        <HashRouter>
-            <Switch>
-                {/* <Route exact path="/"><Redirect to="/resume" /></Route> */}
-                <Route exact path="/" component={App} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/projects" component={Portfolio} />
-                <Route exact path="/contact" component={Contact} />
-            </Switch>
-        </HashRouter>
-    ) : (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/projects" component={Portfolio} />
-                <Route exact path="/contact" component={Contact} />
-            </Switch>
-        </BrowserRouter>
-    )
+    // HashRouter is used for production to avoid issues with routing
+    // when the app is served from a subdirectory
+    // HashRouter uses the hash portion of the URL (window.location.hash)
+    // to keep the UI in sync with the URL
+    return React.createElement(Router, {}, (
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/projects" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/detail" component={Detail} />
+        </Switch>
+    ));
 }
